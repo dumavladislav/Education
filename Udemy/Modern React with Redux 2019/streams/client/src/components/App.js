120692b017215105e3e7e731f42fd8a76ecb4426
Vladislav <dumsky284@mail.ru>
2019-07-18 14:39:36 +0300

    streams app development start

---
+++ Udemy/Modern React with Redux 2019/streams/client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+    return (
+        <div>PageTwo<button>ClickMe!</button></div>
+        
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pagetwo" exact component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App;
